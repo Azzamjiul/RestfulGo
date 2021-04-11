cf3212058bd147d5a00c859430298f6b3825026c
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2021-04-11 20:34:56 +0700

    insert into mysql

--- main.go
+++ main.go
@@ -1,81 +1,28 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type Article struct {
-	Id      string `json:"id"`
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	// fmt.Fprintf(w, "%+v", string(reqBody))
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	Articles = append(Articles, article)
-	json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	for index, article := range Articles {
-		if article.Id == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}
-
 func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Hello description", Content: "Hello content"},
-		Article{Id: "2", Title: "Hello 2", Desc: "Hello 2 description", Content: "Hello 2 content"},
+	fmt.Println("Go Mysql Tutorial")
+
+	db, err := sql.Open("mysql", "root:adminadmin@tcp(127.0.0.1:3306)/golang")
+
+	if err != nil {
+		panic(err.Error())
 	}
-	handleRequests()
+
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO test VALUES (2, 'TEST')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
 }
