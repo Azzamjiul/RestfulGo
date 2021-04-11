523d40216bdf4042741cb6967aad07e722d11ddc
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2021-04-11 20:40:48 +0700

    retreive data from mysql

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type Tag struct {
+	ID   int    `json:id`
+	Name string `json:name`
+}
 
 func main() {
 	fmt.Println("Go Mysql Tutorial")
@@ -18,11 +24,27 @@ func main() {
 
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO test VALUES (2, 'TEST')")
+	// insert, err := db.Query("INSERT INTO test VALUES (1, 'TEST 1')")
 
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// defer insert.Close()
+
+	results, err := db.Query("SELECT id, name FROM test")
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer insert.Close()
+	for results.Next() {
+		var tag Tag
+
+		err = results.Scan(&tag.ID, &tag.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		log.Printf(tag.Name)
+	}
 }
